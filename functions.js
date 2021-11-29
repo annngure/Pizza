7f46ccd832b163fdb25542bd0d2a44453c132fb5
annngure <annwanjikungure@gmail.com>
2021-11-29 23:48:19 +0300

    add cart button

--- functions.js
+++ functions.js
@@ -1,14 +1,13 @@
 //calculate the total for the pizza ordered
-$(document).ready(function(){
-    let pizza =[["small and crispy",550],["medium and crispy",800],["large and crispy",1000],
-                ["small and stuffed",600],["medium and stuffed",850],["large and stuffed",1050],
-                ["small and Gluten-free",700], ["medium and Gluten-free",900],["large and Gluten-free",1200]]
-
-    let drinks=[["coke:small",60],["coke:1ltr",150],["coke:2ltr",290],
-               ["water:300ml",70],["water:500ml",100],
-                [["Delmonte:500ml",250],]["Delmonte:1ltr",550],] 
-    function myFunction(onclick){
-
-    }        
-}
-
+//    let pizza =[["small and crispy",550],["medium and crispy",800],["large and crispy",1000],
+//                ["small and Gluten-free",700], ["medium and Gluten-free",900],["large and Gluten-free",1200]]
+//    let drinks=[["coke:small",60],["coke:1ltr",150],["coke:2ltr",290],
+//                [["Delmonte:500ml",250],]["Delmonte:1ltr",550],] 
+  
+//      console.log("The total is 610")
+  //} else if(pizza[1]+drinks[1]) {
+  //    console.log("The total is")
+  //}
+         
+//}
+//
