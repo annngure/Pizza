6fc4e7cdf556738b5b3e8105e082ee72490eafb1
annngure <annwanjikungure@gmail.com>
2021-11-30 06:37:40 +0300

    change bootstrap link

--- functions.js
+++ functions.js
@@ -1,13 +1,4 @@
-//calculate the total for the pizza ordered
-//    let pizza =[["small and crispy",550],["medium and crispy",800],["large and crispy",1000],
-//                ["small and Gluten-free",700], ["medium and Gluten-free",900],["large and Gluten-free",1200]]
-//    let drinks=[["coke:small",60],["coke:1ltr",150],["coke:2ltr",290],
-//                [["Delmonte:500ml",250],]["Delmonte:1ltr",550],] 
-  
-//      console.log("The total is 610")
-  //} else if(pizza[1]+drinks[1]) {
-  //    console.log("The total is")
-  //}
-         
-//}
-//
+function addtotal(){
+    var pizza = document.getElementById("pizza")
+    
+}
